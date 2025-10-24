13d3a2c4a483e3cc142c7a951f9352a70045de82
ashhwiithac22 <ashwithac22@gmail.com>
2025-10-24 18:21:15 +0530

    frontend for deployment

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  socketUrl: 'https://codebuddy-backend-oind.onrender.com',
+  geminiApiKey: "AIzaSyCwQ_tvwfN_oUkuu0hqJFdzvfbl3hm7XWM",
+  apiUrl: 'https://codebuddy-backend-oind.onrender.com/api'
+};
